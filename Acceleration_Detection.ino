99fc4fbf7b940c7b41124cab7a875f5ed729c605
JamesONeill-III <joneill313@Hotmail.com>
2017-04-08 21:25:13 -0700

    Add files via upload

---
+++ Acceleration_Detection.ino
@@ -0,0 +1,19 @@
+int x,y,z;
+
+void setup() {
+ Serial.begin(9600);
+
+}
+
+void loop() {
+  x = analogRead(0);
+  y = analogRead(1);
+  z = analogRead(2);
+  Serial.print("accelerations are x,y,z: ");
+  Serial.print(x, DEC);
+  Serial.print(" ");
+  Serial.print(y, DEC);
+  Serial.print(" ");
+  Serial.print(z, DEC);
+  delay(200); 
+  }
